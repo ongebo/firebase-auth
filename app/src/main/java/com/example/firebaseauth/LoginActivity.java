d16759082111f5ce2611ac07e88241da06e3ec73
ongebo <isaacongebo@gmail.com>
2019-11-28 20:24:01 +0300

    Add homescreen design

--- app/src/main/java/com/example/firebaseauth/LoginActivity.java
+++ app/src/main/java/com/example/firebaseauth/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.firebaseauth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onSubmitButtonClick(View view) {
+        Intent intent = new Intent(this, HomescreenActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/firebaseauth/SignupActivity.java
+++ app/src/main/java/com/example/firebaseauth/SignupActivity.java
@@ -1,6 +1,8 @@
 package com.example.firebaseauth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,10 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void onSubmitButtonClick(View view) {
+        Intent intent = new Intent(this, HomescreenActivity.class);
+        startActivity(intent);
     }
 }
