aed0a9d690c83789ff4645f402fad6130d8eaa8d
ongebo <isaacongebo@gmail.com>
2019-11-28 19:49:19 +0300

    Add login screen

--- app/src/main/java/com/example/firebaseauth/MainActivity.java
+++ app/src/main/java/com/example/firebaseauth/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.firebaseauth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onLoginButtonClick(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
