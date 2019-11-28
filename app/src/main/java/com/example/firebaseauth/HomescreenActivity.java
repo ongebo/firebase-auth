d6fa202c25670b0355a65bfa4335ab23a0aa7ae8
ongebo <isaacongebo@gmail.com>
2019-11-28 20:41:46 +0300

    Create activity to save new texts

--- app/src/main/java/com/example/firebaseauth/HomescreenActivity.java
+++ app/src/main/java/com/example/firebaseauth/HomescreenActivity.java
@@ -1,6 +1,8 @@
 package com.example.firebaseauth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,10 @@ public class HomescreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
+    }
+
+    public void onAddTextButtonClick(View view) {
+        Intent intent = new Intent(this, AddTextActivity.class);
+        startActivity(intent);
     }
 }
