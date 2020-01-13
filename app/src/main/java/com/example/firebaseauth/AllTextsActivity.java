e8858cde8814f0308284e3d6fb590b155891ebfa
ongebo <isaacongebo@gmail.com>
2020-01-13 13:43:45 +0300

    Fetch and display all texts saved to firestore

--- app/src/main/java/com/example/firebaseauth/AllTextsActivity.java
+++ app/src/main/java/com/example/firebaseauth/AllTextsActivity.java
@@ -1,42 +1,63 @@
 package com.example.firebaseauth;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class AllTextsActivity extends AppCompatActivity {
-    private static List<Text> dummyTexts;
+    public static final String ERROR_MESSAGE = "Could not fetch texts!";
+    private static List<Text> texts;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_texts);
-        initializeDummyTexts();
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new AllTextsAdapter(dummyTexts));
+        texts = new ArrayList<>();
+        recyclerView = findViewById(R.id.recyclerView);
+        fetchAndDisplayTexts();
     }
 
-    private void initializeDummyTexts() {
-        dummyTexts = new ArrayList<>();
-        dummyTexts.add(new Text("One", "Hello One!"));
-        dummyTexts.add(new Text("Two", "Hello Two!"));
-        dummyTexts.add(new Text("Three", "Hello Three!"));
-        dummyTexts.add(new Text("Four", "Hello Four!"));
-        dummyTexts.add(new Text("Five", "Hello Five!"));
-        dummyTexts.add(new Text("Six", "Hello Six!"));
-        dummyTexts.add(new Text("Seven", "Hello Seven!"));
-        dummyTexts.add(new Text("Eight", "Hello Eight!"));
-        dummyTexts.add(new Text("Nine", "Hello Nine!"));
-        dummyTexts.add(new Text("Ten", "Hello Ten!"));
-        dummyTexts.add(new Text("Eleven", "Hello Eleven!"));
-        dummyTexts.add(new Text("Twelve", "Hello Twelve!"));
-        dummyTexts.add(new Text("Thirteen", "Hello Thirteen!"));
+    private void fetchAndDisplayTexts() {
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        OnCompleteListener<QuerySnapshot> onCompleteListener = getOnCompleteListener();
+        firestore.collection("texts")
+                .get()
+                .addOnCompleteListener(onCompleteListener);
+    }
+
+    private OnCompleteListener<QuerySnapshot> getOnCompleteListener() {
+        return new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                        Map<String, Object> map = documentSnapshot.getData();
+                        String title = (String) map.get("title");
+                        String description = (String) map.get("description");
+                        texts.add(new Text(title, description));
+                    }
+                    recyclerView.setHasFixedSize(true);
+                    recyclerView.setLayoutManager(new LinearLayoutManager(AllTextsActivity.this));
+                    recyclerView.setAdapter(new AllTextsAdapter(texts));
+                } else {
+                    Toast.makeText(getApplicationContext(), ERROR_MESSAGE, Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
     }
 }
